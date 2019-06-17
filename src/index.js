40c0d32a1a7cf41bcffa96d5e7e9dfc9f2fad7b2
ironsiuv <s.ammirati95@gmail.com>
2019-06-17 22:08:29 +0200

    - Firebase test

--- src/index.js
+++ src/index.js
@@ -2,8 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import firebase from 'firebase/app';
+
 import * as serviceWorker from './serviceWorker';
 
+
+
+const config = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MSG_SND_ID
+};
+
+firebase.initializeApp(config);
+firebase.firestore().settings({timestampsInSnapshots: true});
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
